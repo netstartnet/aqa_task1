d0b0d9dcf3674780902098f035887815be857b86
netstartnet <gmodkru@gmail.com>
2024-11-25 16:54:35 +0300

    change CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,41 @@
 package ru.netology.service;
 
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
 public class CashbackHackServiceTest {
+
+    @Test
+    public void remainIfAmountEqualBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remainIfAmountLessBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remainIfAmountMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1600);
+        int expected = 400;
+
+        assertEquals(actual, expected);
+    }
 }
