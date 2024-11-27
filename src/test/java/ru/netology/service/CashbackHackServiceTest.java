e4a63db41ca777924ce8ab46cf4503af154d5f5b
netstartnet <gmodkru@gmail.com>
2024-11-28 00:06:25 +0300

    Fixed CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -14,7 +14,7 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(1000);
-        int expected = 1000;
+        int expected = 0;
 
         assertEquals(actual, expected);
     }
